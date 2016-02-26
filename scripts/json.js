b6baa33a2a5723f1a7922ca607a4ec4b31215118
Paul Hayes <fofr@users.noreply.github.com>
2016-02-26 16:20:40 +0000

    Create a JSON file for each post

--- scripts/json.js
+++ scripts/json.js
@@ -1,26 +1,36 @@
 hexo.extend.generator.register(hexo_generator_json_content);
 
 function hexo_generator_json_content(site) {
-  var json = {},
-      content;
+  var routes = [],
+      all;
 
-  content = site.posts.sort('-date').filter(function (post) {
-    return post.published;
-  }).map(function (post) {
+  all = site.posts.sort('-date').map(function(post) {
+    return postObject(post);
+  });
+
+  routes = site.posts.sort('-date').map(function(post) {
+    return {
+      path: post.path.replace(/\/$/, "") + '.json',
+      data: JSON.stringify(postObject(post))
+    };
+  });
+
+  routes.push({
+    path: 'api/all.json',
+    data: JSON.stringify({posts: all})
+  });
+
+  return routes;
+
+  function postObject(post) {
     return {
       title: post.title,
       date: post.date,
       updated: post.updated,
-      path: post.path,
+      path: '/' + post.path,
       permalink: post.permalink,
-      content: post.content
-    };
-  });
-
-  json.posts = content;
-
-  return {
-    path: 'api/all.json',
-    data: JSON.stringify(json)
-  };
+      content: post.content,
+      file: hexo.config.image_dir + '1720/' + post.slug + '.jpg'
+    }
+  }
 }
