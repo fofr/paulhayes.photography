60438abe7ac8d921c4d36a5a98ac8d6c9f37d230
Paul Hayes <fofr@users.noreply.github.com>
2016-05-15 19:46:07 +0100

    Tweak exif data extraction
    * Use original date time rather than modified date time
    * Use Time based assertions when deciding lens, based on purchase dates
    (new lens becomes choice over older lens of similar focal length)
    * Re-write shutter, strip of `.0` for readability, keep shutters `1/XX`
    if they begin with `1`, otherwise use decimals (eg 2/3 is a harder to
    read shutter speed)

--- generators/exif-helper.rb
+++ generators/exif-helper.rb
@@ -1,6 +1,8 @@
+require 'time'
+
 module ExifHelper
   def date(exif)
-    exif.date_time.strftime('%Y-%m-%d %H:%M')
+    exif.date_time_original.strftime('%Y-%m-%d %H:%M')
   end
 
   def camera(exif)
@@ -18,8 +20,14 @@ module ExifHelper
       lens = "Canon EF 70-200mm f/4L IS"
     elsif focal_length == 50
       lens = "Canon EF 50mm f/1.4 USM"
-    elsif focal_length > 16.9
+    elsif focal_length >= 17 && Time.parse('2015-01-01') < exif.date_time_original
+      # Purchased start of 2015
       lens = "Canon EF 17-40mm f/4L USM"
+    elsif focal_length >= 23
+      lens = "Canon EF-S 17-55mm f/2.8 IS USM"
+    elsif focal_length >= 18 && Time.parse('2013-01-01') > exif.date_time_original
+      # Kits lens, stopped using circa 2013
+      lens = "Canon EF-S 18-55mm f/3.5-5.6 IS"
     else
       lens = "Canon EF-S 10-22mm f/3.5-4.5 USM"
     end
@@ -32,14 +40,17 @@ module ExifHelper
   # Readable shutter speeds
   # Use 0.X but not 1.X or 0.0X shutter speeds
   def shutter(exif)
-    exposure_string = exif.exposure_time
-    exposure_float = exposure_string.to_f
+    shutter = exif.exposure_time.to_f
 
-    if exposure_float < 1 && exposure_float > 0.09
-      exposure_float
-    else
-      exposure_string
+    if exif.exposure_time && exif.exposure_time.numerator != 1 && shutter < 1
+      return shutter
     end
+
+    if shutter < 1
+      return exif.exposure_time
+    end
+
+    shutter.to_s.sub(/\.0$/,'')
   end
 
   def iso(exif)
