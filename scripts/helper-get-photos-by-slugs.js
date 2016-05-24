ac44d6063824bfc4e8cd7f79b719f57457cd2193
Paul Hayes <fofr@users.noreply.github.com>
2016-05-24 09:06:55 +0100

    Ignore photos prefix in `find by slug` helper

--- scripts/helper-get-photos-by-slugs.js
+++ scripts/helper-get-photos-by-slugs.js
@@ -3,7 +3,7 @@ hexo.extend.helper.register('getPhotosBySlug', function(slugsArray){
       hexo = this;
 
   for (var i = 0, l = slugsArray.length; i < l; i++) {
-    var slug = slugsArray[i],
+    var slug = 'photos/' + slugsArray[i],
         post = hexo.site.posts.findOne({slug: slug});
 
     if (post) {
