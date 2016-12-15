c588f1544af3a18d00019da9eb117805fa1f2e45
Paul Hayes <fofr@users.noreply.github.com>
2016-12-15 21:13:03 +0000

    Make email a mailto link

--- scripts/tag-email.js
+++ scripts/tag-email.js
@@ -1,4 +1,4 @@
 /* Usage: {% contact %} */
 hexo.extend.tag.register('contact', function(args, content, options){
-  return '<br /><b>' + hexo.config.site_email + '</b>';
+  return '<br /><b><a href="mailto:' + hexo.config.site_email + '">' + hexo.config.site_email + '</a></b>';
 });
